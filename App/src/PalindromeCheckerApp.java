a4a6a04cc1958cfe1d0f0134c8d8e3d713354432
dharshan007x <dharshanbaalakrishnan04@gmail.com>
2026-03-04 22:19:41 +0530

    dharshan added usecase1

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,11 +1,15 @@
-// version 1.0
-//author Abc
-
-//UseCase 1: Welcome page
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+        System.out.println("====================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("====================================");
+
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("------------------------------------");
+        System.out.println("Application started successfully.");
+        System.out.println("------------------------------------");
     }
-
 }
